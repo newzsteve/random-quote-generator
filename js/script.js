c948d0dff9d05626f418a36c93c5a64bb643f325
newzsteve <63953401+newzsteve@users.noreply.github.com>
2020-04-19 13:21:54 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -164,7 +164,7 @@ var quotesUsed = [];
 var intervalID;
 
 // const keyword is not supported in IE
-var NUMBER_OF_QUOTES = 6;
+var NUMBER_OF_QUOTES = 38;
 var CHANGE_INTERVAL = 1000000;
 
 // From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
