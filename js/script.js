c82f332c5a44c435070810b8597b09b62aca15a7
newzsteve <63953401+newzsteve@users.noreply.github.com>
2020-04-19 16:52:09 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -345,6 +345,402 @@ var quotes = [
 {
     quote: "If you could speak to anyone in heaven, who would it be?",
     source: "50 Questions That Will Make You Fall Even Deeper In Love"
+},
+{
+	quote: "What are the most important things on your personal bucket list?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "What's something that you'd like to try, but that you're too scared to try?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "If all of your friends were asked to describe you, which friend’s description would be the most accurate and why?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "What was your favorite book (or movie) as a child and why did you like it so much?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "What's the one thing about yourself that you would most like to change?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "What would be your dream vacation?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "What are three qualities you admire about yourself?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "What are three qualities you admire about your partner?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "What's one of your best memories from when you and your partner were dating?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "What was your favorite place to go as a child and why did you love it there?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "If you could be any character in a book (or movie), who would you choose to be?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "What three things in your life could you not imagine doing without?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "If someone gave you enough money to start a business—no strings attached—what kind of business would you want to start?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "If you could possess one artistic talent (painting, sculpture, composing music, writing, etc.), which talent would you choose and why?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "If you could be an Olympic or professional athlete, what sport would you choose and why?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "When you were a kid, did you think about having kids yourself one day? If so, how many and what did you imagine doing with them?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "What's one of the most adventurous things you have ever done?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "What do you dream about the most often?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "Which of your parents do you think you're most like and why?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "What was the first thing about your partner that attracted you?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "What do you like most about your appearance?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "Over the last five years, how do you think you have changed?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "If you could take a year-long paid sabbatical, what would you do?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "If you had to guess right now, what do you think our kid(s) would choose as a career someday and why?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "When you think about the future, what do you imagine?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "Do you learn new skills or information best by hearing, seeing, or doing?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "If you could plan the perfect date with me, what would that include?",
+	source: "40 Questions to Help Build Intimacy in a Relationship"
+},
+{
+	quote: "What Are Three Things You Love About Your Partner, And One Thing They Can Improve On?",
+	source: "11 Questions To Ask Your Partner, Bustle"
+},
+{
+	quote: "What Do You Find It Difficult To Open Up To Your Partner About?",
+	source: "11 Questions To Ask Your Partner, Bustle"
+},
+{
+	quote: "What Can Your Partner Do To Help Deepen Your Trust In Them?",
+	source: "11 Questions To Ask Your Partner, Bustle"
+},
+{
+	quote: "Are There Any Parts Of You That You Feel Get Left Out Of Our Relationship?",
+	source: "11 Questions To Ask Your Partner, Bustle"
+},
+{
+	quote: "Describe A Time When You Felt Like Your Partner Really Saw You For Who You Are",
+	source: "11 Questions To Ask Your Partner, Bustle"
+},
+{
+	quote: "What Are Some Annoying Habits Of Other Couples That Irritate You The Most?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "What Kind Of A Parent Do You Think You’d Be?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "How Content Are You With The Amount Of Non-Sexual Affection In The Relationship?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "You and Your Partner Are Hitting The Great Outdoors. What Would You Prefer To Spend The Day Doing?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "If You Could Ask Your Partner Anything And Have Them Answer Truthfully, What Would It Be?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "What Is The Best “Relationship Advice” Someone Has Ever Given You?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "Are You Happy With The Amount Of Us-Time And Apart-Time You Have With Your Partner?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "Are You Friends With Any Of Your Exes?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "Would You Lie To Make Your Partner Happy And Where Would You Draw The Line?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "Are There Any Times You Feel Lonely, Left Out Or Like Your Partner Is Not Reachable Emotionally?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "What Was The Best Moment In Your Relationship So Far?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "What Are The Biggest Differences Between You And Your Partner?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "What Is One Area In Your Life Where You Feel Like Something Is Missing?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "Do You Think Your Partner Can Be Both Your Best Friend And Your Lover?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "Would You Rather Stay In Or Go Out For A Date?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "Would You Rather Be Known For Your Intelligence Or Your Good Looks?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "Would You Rather Be Proposed To In Private Or In Front Of Family And Friends?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "What Would You Define As Cheating?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "Would You Compromise Your Happiness For The Success Of The Relationship?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "Do You Think Past Relationship Secrets Should Always Be Kept Hidden?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "What’s The One Thing About Your Partner You’d Like To Change?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "Do You Think A Couple’s Finances Should Be Together Or Separate?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "What Are Your Biggest Fears About Relationships?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "What Lessons Have You Learned From Past Relationships?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "Upon Dying, If You Had The Choice To Re-Live Your Life Or Live A Different Life, Which Would You Choose?",
+	source: "53 Lifehack Questions"
+},
+{
+	quote: "What’s your favourite part of your partner's body?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "If you could choose what your partner was wearing right now, what would you choose?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "Where on your body is your favourite place to be touched?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s the best sex you’ve ever had?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s your favourite position?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "Do you prefer to give or receive?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What was your best orgasm ever?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "If you were out for dinner with your partner and they said they wanted to have sex right now, what would you do?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "Have you ever sent nudes of yourself?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s the first thing that sexually attracts you to someone?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "Would you say you have any fetishes?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "Do you ever read erotic fiction?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "Do you enjoy shower sex?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "Have you ever had an inappropriate crush?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "Do you get tired after sex?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s the longest you’ve ever gone without sex?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s a surefire way to turn you on?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "Do you like dirty talk?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "Do you prefer to take control or be submissive?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "Do you prefer one night stands or longer term sexual partners?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "Do you prefer to be on top or bottom?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What would you like to do more of in bed?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s your favourite time of day to have sex?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "Rough or romantic? Quickie or marathon session?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s your favourite part of foreplay?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "Would you dress up for your partner in bed?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s your favourite place to be kissed?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s the best thing about your sex life?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s the shortest time it’s ever taken for you to have an orgasm? What's the longest time it’s ever taken for you to have an orgasm?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "How would you make your partner have an orgasm?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What's the first thing you’d do if you woke up one day and you were the opposite sex?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What sexual activity do you consider totally off limits?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s the biggest romantic fail you’ve ever experienced?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s the biggest lie you’ve ever told without getting caught?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "If you could erase one past experience, what would it be?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "When’s the last time you were flat-out rejected and how did you handle it?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s your biggest sexual fear?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s your favorite go-to move for getting attention from the opposite sex?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s the most flirtatious thing you’ve ever done?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s the one thing you’d do in bed with someone if you had no inhibitions whatsoever?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s the most surprising thing that’s ever turned you on?",
+	source: "42 sex questions to ask your partner"
+},
+{
+	quote: "What’s the biggest dating disappointment you’ve experienced so far?",
+	source: "42 sex questions to ask your partner"
 }
 ];
 
